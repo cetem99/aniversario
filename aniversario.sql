8656058c61b2b64bb33410995e75048109193013
guiopadilha <149798569+guiopadilha@users.noreply.github.com>
2024-09-11 22:36:00 -0300

    Atualização and rename aniversario_aniversariantes.sql to aniversario.sql

--- aniversario.sql
+++ aniversario.sql
@@ -1,10 +1,7 @@
-CREATE DATABASE  IF NOT EXISTS `aniversario` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `aniversario`;
+CREATE DATABASE IF NOT EXISTS `lembrar_aniversarios` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `lembrar_aniversarios`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: aniversario
--- ------------------------------------------------------
--- Server version	8.3.0
+-- Host: 127.0.0.1    Database: lembrar_aniversarios
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,9 +14,7 @@ USE `aniversario`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
 -- Table structure for table `aniversariantes`
---
 
 DROP TABLE IF EXISTS `aniversariantes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -27,13 +22,12 @@ DROP TABLE IF EXISTS `aniversariantes`;
 CREATE TABLE `aniversariantes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
+  `data_aniversario` date NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
 -- Dumping data for table `aniversariantes`
---
 
 LOCK TABLES `aniversariantes` WRITE;
 /*!40000 ALTER TABLE `aniversariantes` DISABLE KEYS */;
@@ -49,4 +43,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-10 15:21:28
+-- Dump completed on 2024-09-11
