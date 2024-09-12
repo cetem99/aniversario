759b1cc790d459617e3c61a43974a89693f8fb04
guiopadilha <149798569+guiopadilha@users.noreply.github.com>
2024-09-11 22:32:38 -0300

    Atualização create_aniversariante.py

--- create_aniversariante.py
+++ create_aniversariante.py
@@ -3,27 +3,28 @@ import mysql.connector
 
 app = Flask(__name__)
 
-def criar_aniversariante(nome):
+def criar_aniversariante(nome, data_aniversario):
     conexao = mysql.connector.connect(
         host='localhost',
         user='root',
-        password='',
-        database='aniversario'
+        password='12345678',
+        database='lembrar_aniversarios'
     )
     cursor = conexao.cursor()
 
-    comando = f'INSERT INTO aniversariantes (nome) VALUES ("{nome}")' 
+    comando = f'INSERT INTO aniversariantes (nome, data_aniversario) VALUES ("{nome}", "{data_aniversario}")'
     cursor.execute(comando)
     conexao.commit()
 
     cursor.close()
     conexao.close()
 
-@app.route('/aniversariante', methods=['GET', 'POST'])
+@app.route('/lembrar_aniversariante', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         nome = request.form['nome']
-        criar_aniversariante(nome)
+        data_aniversario = request.form['data_aniversario']  # Captura a data do formulário
+        criar_aniversariante(nome, data_aniversario)
         return "Aniversariante criado com sucesso!"
     else:
         return render_template('formulario.html')
