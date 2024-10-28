60a449f452e68f07e7adf95c5b876a36acf043bd
guiopadilha <149798569+guiopadilha@users.noreply.github.com>
2024-10-28 18:01:47 -0300

    Update aniversario.sql

--- aniversario.sql
+++ aniversario.sql
@@ -1,15 +1,6 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema lembrar_aniversarios
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema lembrar_aniversarios
@@ -33,26 +24,29 @@ AUTO_INCREMENT = 5
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
 -- -----------------------------------------------------
 -- Table `lembrar_aniversarios`.`aniversariantes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lembrar_aniversarios`.`aniversariantes` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(255) NOT NULL,
-  `data_aniversario` DATE NOT NULL,
+  `data_aniversario` DATE NOT NULL DEFAULT '2000-01-01',
   `email` VARCHAR(255) NOT NULL,
   `telefone` VARCHAR(20) NOT NULL,
   `notificacao` VARCHAR(50) NOT NULL,
   `felicitacao` TEXT NULL DEFAULT NULL,
   `usuario_id` INT NOT NULL,
-  PRIMARY KEY (`id`, `usuario_id`),
-  INDEX `fk_aniversariantes_usuario_idx` (`usuario_id` ASC) VISIBLE)
-ENGINE = MyISAM
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
+  PRIMARY KEY (`id`),
+  INDEX `fk_aniversariantes_usuario_idx` (`usuario_id` ASC),
+  CONSTRAINT `fk_aniversariantes_usuario`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `usuario` (`id`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+select * from usuario;
